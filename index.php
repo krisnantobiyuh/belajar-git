8e24e4137c62abb5a2926ed5d5e0c1273a97e1c8
krisnanto <krisnanto@k15.com>
2017-07-27 09:52:28 +0700

    perubahan 1

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<div class="content">
+
+		</div>
+	</body>
+</html>
+
 <?php
 	echo"hello git ";
 
 	echo "krinanto belajar git";
+	echo "bersama";
